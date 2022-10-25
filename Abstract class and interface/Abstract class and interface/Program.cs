c3c27f64528ca80e2fd4cd493c7275822fe508bf
kchau <90921523+cybkate@users.noreply.github.com>
2022-10-26 01:41:19 +0300

    Abstract-class-and-interface

---
+++ Abstract class and interface/Abstract class and interface/Program.cs
@@ -0,0 +1,27 @@
+﻿abstract class Geographical_Object
+{
+    protected double CoordinateX;
+    protected double CoordinateY;
+    protected string name;
+    protected string description;
+
+    protected virtual void getInformation() 
+    {
+    }
+}
+
+class River : Geographical_Object
+{
+    double speed;
+    double general_length;
+
+    public void getInformation()
+    {
+        Console.WriteLine($"Назва річки {name}, координати якої {CoordinateX};{CoordinateY}, зі швидкістю {speed}, а загальна довжина {general_length}");
+    }
+}
+
+class Mountain : Geographical_Object
+{
+    double highestPoint;
+}
---
+++ Abstract class and interface/Interface/Program.cs
@@ -0,0 +1,126 @@
+﻿using System.Xml.Linq;
+
+interface IGeographical_Object
+{
+    double IXcoordinate { get; set; }
+    double IYcoordinate { get; set; }
+    string Name { get; set; }
+    string Description { get; set; }
+    void getInformation() { }
+}
+
+class River : IGeographical_Object
+{
+    double speed;
+    double length;
+    double CoordinateX;
+    double CoordinateY;
+    string name;
+    string description;
+
+    public double IXcoordinate 
+    { get 
+        { 
+            return CoordinateX; 
+        } 
+      set 
+        { 
+            CoordinateX = value; 
+        } 
+    }
+    public double IYcoordinate 
+    { 
+        get 
+        { 
+            return CoordinateY; 
+        } 
+        set 
+        { 
+            CoordinateY = value; 
+        } 
+    }
+    public string Name 
+    { 
+        get 
+        { 
+            return name; 
+        } 
+        set 
+        { 
+            name = value; 
+        } 
+    }
+    public string Description 
+    { 
+        get 
+        { 
+            return description; 
+        } 
+        set 
+        { 
+            description = value; 
+        } 
+    }
+    void IGeographical_Object.getInformation()
+    {
+        Console.WriteLine($"Назва річки {name}, координати якої {CoordinateX};{CoordinateY}, зі швидкістю {speed}, а її довжина {length}");
+    }
+}
+
+class Mountain : IGeographical_Object
+{
+    double highestPoint;
+    double CoordinateX;
+    double CoordinateY;
+    string name;
+    string description;
+
+    public double IXcoordinate 
+    { 
+        get 
+        { 
+            return CoordinateX; 
+        } 
+        set 
+        { 
+            CoordinateX = value; 
+        } 
+    }
+    public double IYcoordinate 
+    { 
+        get 
+        { 
+            return CoordinateY; 
+        } 
+        set 
+        { 
+            CoordinateY = value; 
+        } 
+    }
+    public string Name 
+    { 
+        get 
+        { 
+            return name; 
+        } 
+        set 
+        { 
+            name = value; 
+        } 
+    }
+    public string Description 
+    { 
+        get 
+        { 
+            return description; 
+        } 
+        set 
+        { 
+            description = value; 
+        } 
+    }
+    void IGeographical_Object.getInformation()
+    {
+        Console.WriteLine($"Назва гори {name}, яка знаходиться на {CoordinateX};{CoordinateY}, а найвища точка {highestPoint}");
+    }
+}
